0310d22a796742231b786344e49101886657c662
Aman <aman@parallelhq.com>
2023-02-02 17:50:15 +0530

    pixi function

---
+++ heroAnimation.js
@@ -0,0 +1,78 @@
+// PIXI JS Config FIle
+
+const { Application, Geometry, Mesh, Texture, Shader } = PIXI;
+const assets = {
+  texture:
+    "https://uploads-ssl.webflow.com/6398244f3e820011a013d22a/63bba36d076d75e1dc7872dd_63b67bd5b51823b3b1aa3748_blobs%20bg%20with%20less%20(1).png",
+};
+const canvas = document.getElementById("mycanvas");
+
+const app = new Application({
+  view: canvas,
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
+
+const geometry = new Geometry();
+geometry.addAttribute(
+  "aVertexPosition",
+  [-100, -100, 100, -100, 100, 100, -100, 100],
+  2
+);
+geometry
+  .addAttribute("aUvs", [0, 0, 1, 0, 1, 1, 0, 1], 2)
+  .addIndex([0, 1, 2, 0, 2, 3]);
+
+const vertexSrc = `
+
+    precision mediump float;
+
+    attribute vec2 aVertexPosition;
+    attribute vec2 aUvs;
+
+    uniform mat3 translationMatrix;
+    uniform mat3 projectionMatrix;
+
+    varying vec2 vUvs;
+
+    void main() {
+
+        vUvs = aUvs;
+        gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+
+    }`;
+
+const fragmentSrc = `
+
+    precision mediump float;
+
+    varying vec2 vUvs;
+
+    uniform sampler2D uSampler2;
+    uniform float time;
+
+    void main() {
+
+        gl_FragColor = texture2D(uSampler2, vUvs + sin( (time + (vUvs.x) * 14.) ) * 0.1 );
+    }`;
+
+const uniforms = {
+  uSampler2: Texture.from(assets.texture),
+  time: 0,
+};
+
+const shader = Shader.from(vertexSrc, fragmentSrc, uniforms);
+
+const quad = new Mesh(geometry, shader);
+
+quad.position.set(
+  document.documentElement.clientWidth / 2,
+  window.innerHeight / 2
+);
+quad.scale.set(10);
+
+app.stage.addChild(quad);
+
+app.ticker.add((delta) => {
+  quad.shader.uniforms.time += 0.01;
+});
